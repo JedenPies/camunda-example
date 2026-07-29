f385b76e487532640c8e307b7530133a5df1837f
Patryk Dobrowolski <patryk.dobrowolski@gmail.com>
2026-07-29 10:29:09 +0200

    dir structure fixed + gradle configuration

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {}
+
+tasks.register<Exec>("buildConnector") {
+    workingDir = file("camunda-connector")
+    commandLine("mvn.cmd", "clean", "package")
+}
+
+tasks.register<Exec>("buildJobWorker") {
+    workingDir = file("camunda-job-worker")
+    commandLine("gradlew.bat", "bootJar")
+}
+
+tasks.register<Exec>("dockerComposeUp") {
+    dependsOn("buildConnector", "buildJobWorker")
+    commandLine("docker-compose", "up", "-d")
+}
